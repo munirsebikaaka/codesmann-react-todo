7f1d6bbfb169d77ab3ef6487ce473f6bb15f5736
munir sebikaaka <munirsebikaaka@gmail.com>
2024-09-13 13:46:49 +0300

    feat:added a function tha pushes a task from the form to the new todos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [todo, setTodo] = useState("");
+  const [todoNew, setTodoNew] = useState([]);
+
+  function hundleTodosInFormComponet(e) {
+    e.preventDefault();
+    todoNew.push({ task: todo, addB: "TIC", remB: "EX" });
+    setTodo("");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <FormTodo
+        onTodo={todo}
+        onSetTodo={setTodo}
+        onHundleTodosInFormComponet={hundleTodosInFormComponet}
+      />
+      <NewToDos onNewTodos={todoNew} />
     </div>
+  );
+}
+function FormTodo({ onTodo, onSetTodo, onHundleTodosInFormComponet }) {
+  return (
+    <div>
+      <h1 className="todoname">The react todo app</h1>
+      <form className="todoform" onSubmit={onHundleTodosInFormComponet}>
+        <input
+          type="text"
+          value={onTodo}
+          onChange={(e) => onSetTodo(e.target.value)}
+          placeholder="Pleaase input task"
+        />
+        <button>ADD</button>
+      </form>
+    </div>
+  );
+}
+function NewToDos({ onNewTodos }) {
+  return (
+    <ul>
+      {onNewTodos.map((el) => (
+        <li key={el.task}>
+          {el.task}
+          <button>{el.addB}</button>
+          <button>{el.remB}</button>
+        </li>
+      ))}
+    </ul>
   );
 }
 
