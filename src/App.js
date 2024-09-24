b793ac4024321fee0095c34426659465459124fe
munir sebikaaka <munirsebikaaka@gmail.com>
2024-09-24 13:01:26 +0300

    finished the styles for the application

--- src/App.js
+++ src/App.js
@@ -92,12 +92,13 @@ function App() {
 function Form({ onTodo, onSetTodo, onPushToNewTodos }) {
   return (
     <div>
-      <h1>todo new version</h1>
+      <h1 className="form-head">the react todo App</h1>
 
       <form className="todo-form" onSubmit={onPushToNewTodos}>
         <input
           className="type-input"
           type="text"
+          placeholder="Please input your todo."
           value={onTodo}
           onChange={(e) => onSetTodo(e.target.value)}
         />
@@ -111,21 +112,28 @@ function NewTasks({ onTodoNew, onPushToComplete, onPushToCancelledFromNew }) {
   return (
     <div className="level-cell">
       <h1 className="heading heading-1">new todos</h1>
-      <ul>
-        {onTodoNew.map((el) => (
-          <li key={el.id}>
-            {el.task}
-            <div>
-              <button className="btn btn-1" onClick={onPushToComplete}>
-                {el.addB}
-              </button>
-              <button className="btn btn-2" onClick={onPushToCancelledFromNew}>
-                {el.remB}
-              </button>
-            </div>
-          </li>
-        ))}
-      </ul>
+      {onTodoNew.length > 0 ? (
+        <ul>
+          {onTodoNew.map((el) => (
+            <li key={el.id}>
+              {el.task}
+              <div>
+                <button className="btn btn-1" onClick={onPushToComplete}>
+                  {el.addB}
+                </button>
+                <button
+                  className="btn btn-2"
+                  onClick={onPushToCancelledFromNew}
+                >
+                  {el.remB}
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <h1 className="default">no todos available</h1>
+      )}
     </div>
   );
 }
@@ -133,21 +141,25 @@ function CompleteTasks({ onCompleteTask, onPushToCancelled }) {
   return (
     <div className="level-cell">
       <h1 className="heading heading-2">complete todos</h1>
-      <ul>
-        {onCompleteTask.map((el) => (
-          <li key={el.task}>
-            {el.task}
-            <div>
-              <button className="btn btn-1" onClick={onPushToCancelled}>
-                {el.addB}
-              </button>
-              <button className="btn btn-2" onClick={onPushToCancelled}>
-                {el.remB}
-              </button>
-            </div>
-          </li>
-        ))}
-      </ul>
+      {onCompleteTask.length > 0 ? (
+        <ul>
+          {onCompleteTask.map((el) => (
+            <li key={el.task}>
+              {el.task}
+              <div>
+                <button className="btn btn-1" onClick={onPushToCancelled}>
+                  {el.addB}
+                </button>
+                <button className="btn btn-2" onClick={onPushToCancelled}>
+                  {el.remB}
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <h1 className="default">no todo available</h1>
+      )}
     </div>
   );
 }
@@ -155,21 +167,25 @@ function CancelledTodos({ onCancelled, onRedoTodo, onDeleteTodo }) {
   return (
     <div className="level-cell">
       <h1 className="heading heading-3">Cancelled todos</h1>
-      <ul>
-        {onCancelled.map((el) => (
-          <li key={el.task}>
-            {el.task}
-            <div>
-              <button className="btn btn-1" onClick={onRedoTodo}>
-                {el.addB}
-              </button>
-              <button className="btn btn-2" onClick={onDeleteTodo}>
-                {el.remB}
-              </button>
-            </div>
-          </li>
-        ))}
-      </ul>
+      {onCancelled.length > 0 ? (
+        <ul>
+          {onCancelled.map((el) => (
+            <li key={el.task}>
+              {el.task}
+              <div>
+                <button className="btn btn-1" onClick={onRedoTodo}>
+                  {el.addB}
+                </button>
+                <button className="btn btn-2" onClick={onDeleteTodo}>
+                  {el.remB}
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <h1 className="default">no todo available</h1>
+      )}
     </div>
   );
 }
